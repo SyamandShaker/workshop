c28da365968feeb149cec1a192642591a079b584
SyamandShaker <syamand.shaker@powercoders.org>
2020-05-06 10:16:50 +0200

    update my learning folder

--- Challenges/2020.04.28/Return the Remainder from Two Numbers/app.js
+++
@@ -1,9 +0,0 @@
-function remainder(x, y) {
-
-  return x % y
-
-}
-show(9 % 4);
-show(-12 % 5);
-show(-4 % 2);
-show(5.5 % 3);
---
+++ JS/Learning/10.DOM-Everything/scripts.js
@@ -0,0 +1,47 @@
+// →→→ HELPER FUNCTION → TARGET ANY SELECTOR
+
+
+
+// →→→ HELPER FUNCTION → CREATE ANY ELEMENT
+
+
+
+// Target element → classic approach
+
+
+// Target element → awesome approach
+
+
+// →→→ CREATE ELEMENT [Classic approach]
+
+
+// →→→ ADD TEXT CONTENT
+
+
+// →→→ ADD HTML CONTENT
+
+
+// →→→ YOU CAN DO MORE WITH HTML CONTENT
+// insertAdjacentHTML()
+// The four positions available are:
+// "beforebegin" (directly before the current node)
+// "afterbegin" (inside the current node, at the beginning)
+// "beforeend" (inside the current node, at the end)
+// "afterend" (directly after the current node)
+
+
+// This will only touch the first item of his kind
+
+
+// This will touch all of them
+
+
+
+// →→→ DO STYLES
+
+
+
+// →→→ INSERT, APPEND, PREPEND
+
+
+
